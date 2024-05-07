b03168524fb278748a6c51f35299a12bec9241fb
Caleb <87406555+caleblopez96@users.noreply.github.com>
2024-05-07 00:48:04 -0700

    Add files via upload

--- src/script.js
+++ src/script.js
@@ -1,9 +1,13 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
+const main = document.querySelector('main');
+const footer = document.querySelector('footer');
 
 hamburger.addEventListener("click", mobileMenu);
 
 function mobileMenu() {
   hamburger.classList.toggle("active");
-  navMenu.classList.toggle("active");
+	navMenu.classList.toggle("active");
+  main.classList.toggle('blur');
+  footer.classList.toggle('blur');
 }
